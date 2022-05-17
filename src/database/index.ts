a1a499473c38edce48a3f3ccd090a3bd4ce0e1f3
jeanknieling <jeanknieling@gmail.com>
2022-05-17 10:44:27 -0300

    Begin a project

---
+++ src/database/index.ts
@@ -0,0 +1,10 @@
+const users = [
+  {
+    id: "123456",
+    name: "Jean Knieling",
+    email: "jean@gmail",
+    password: "123456",
+    created_at: "2022-05-17",
+    updated_at: "2022-05-17"
+  }
+];
