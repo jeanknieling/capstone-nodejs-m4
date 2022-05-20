310556b15487d03f765f1ccef5414f3dfd53e261
jeanknieling <jeanknieling@gmail.com>
2022-05-20 15:00:56 -0300

    ✅ update: validation users fields with yup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,14 +7,31 @@ import userLoginController from "../controllers/user/userLogin.controller";
 import userUpdateController from "../controllers/user/userUpdate.controller";
 import { authUser } from "../middlewares/user/authUser.middleware";
 import { verifyisAdmMiddleware } from "../middlewares/user/authUser.middleware";
+import { expressYupMiddleware } from "express-yup-middleware";
+import createUserSchema from "../validations/users/createUser.validation";
+import userLoginSchema from "../validations/users/userLogin.validation";
+import updateUserSchema from "../validations/users/updateUser.validation";
 
 const routes = Router();
 
-routes.post("/users", userCreateController);
-routes.post("/users/login", userLoginController);
+routes.post(
+  "/users",
+  expressYupMiddleware({ schemaValidator: createUserSchema }),
+  userCreateController
+);
+routes.post(
+  "/users/login",
+  expressYupMiddleware({ schemaValidator: userLoginSchema }),
+  userLoginController
+);
 routes.get("/users", userListController);
-routes.get("/users/:id", authUser, userListOneController);
-routes.delete("/users/:id", authUser, userDeleteSelfController);
-routes.patch("/users/:id", authUser, userUpdateController);
+routes.get("/users/me", authUser, userListOneController);
+routes.delete("/users/me", authUser, userDeleteSelfController);
+routes.patch(
+  "/users/:id",
+  expressYupMiddleware({ schemaValidator: updateUserSchema }),
+  authUser,
+  userUpdateController
+);
 
 export default routes;
