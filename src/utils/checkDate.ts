31982a1fc1555a2ca490baf819157fc2295f508f
Hamart Rony Lucena Rosa <68301688+hamartrony@users.noreply.github.com>
2022-05-23 10:19:10 -0300

    Revert "Feature/tests"

--- src/utils/checkDate.ts
+++
@@ -1,9 +0,0 @@
-export const checkDate = (date: Date) => {
-  const dateString = date.toString();
-  let exp = /\d{4}\-\d{2}\-\d{2}/;
-  if (!exp.test(dateString)) {
-    Error("Invalid date");
-    return false;
-  }
-  return true;
-};
