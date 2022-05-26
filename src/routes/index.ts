9cd590d949e5db84068ac0e1d78a523be545f9dd
wellery <wellerychaves@outlook.com>
2022-05-26 08:58:22 -0300

    ✅ Pass Tests: all tests are ready.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,8 @@ import { categoriesRoutes } from "./categories.routes";
 import { addressRoutes } from "./address.routes";
 
 export const appRoutes = (app: Express) => {
-  app.use("/users", usersRoutes());
-  app.use("/products", productsRoutes());
-  app.use("/address", addressRoutes());
-  app.use("/category", categoriesRoutes());
-
+    app.use("/users", usersRoutes());
+    app.use("/products", productsRoutes());
+    app.use("/address", addressRoutes());
+    app.use("/category", categoriesRoutes());
 };
